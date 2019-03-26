0f8ba56d40afaedc10b3358578aca849cf580cc2
iankabugi <iank299@gmail.com>
2019-03-26 20:14:09 +0300

    add templates to the project

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
