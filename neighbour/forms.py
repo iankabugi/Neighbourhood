fbbd308b780462a9385a1f384f97860568b81e4e
iankabugi <iank299@gmail.com>
2019-03-26 21:49:06 +0300

    align code according to pep8 secifications for easy code readability

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -3,35 +3,37 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Neighbour, Business, Posts
 
+
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        
+
+
 class CreateHoodForm(forms.ModelForm):
-    """
+    '''
     Model form class to create a neighbourhood
-    """
+    '''
     class Meta:
         model = Neighbour
-        exclude =['user'] 
+        exclude = ['user']
 
 
 class CreateBizForm(forms.ModelForm):
-    """
+    '''
     Model form class to create a neighbourhood
-    """
+    '''
     class Meta:
         model = Business
-        exclude =['user','hood'] 
+        exclude = ['user', 'hood']
 
 
 class CreatePostForm(forms.ModelForm):
-    """
+    '''
     Model form class to create posts
-    """
+    '''
     class Meta:
         model = Posts
-        exclude =['user','hood']
+        exclude = ['user', 'hood']
