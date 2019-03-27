6fdea4743153cf6cdbee4bc92149e70bef14b989
iankabugi <iank299@gmail.com>
2019-03-27 10:11:26 +0300

    final push

--- neighbour/models.py
+++ neighbour/models.py
@@ -4,26 +4,24 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
 class Neighbour(models.Model):
     """
     Class that defines neighbourhood details
     """
-    name = models.CharField(max_length = 60)
+    name = models.CharField(max_length=60)
     loc = (
-    ('Westlands', 'Westlands'),
-    ('South b', 'South b'),
-    ('South c', 'South c'),
-    ('Langata', 'Langata'),
-    
-)
-    description = models.TextField(default="My hood is cool")
-    location = models.CharField(max_length = 60, choices=loc)
+        ('Rongai', 'Rongai'),
+        ('Westlands', 'Westlands'),
+        ('South b', 'South b'),
+        ('South c', 'South c'),
+        ('Langata', 'Langata'),
+
+    )
+    description = models.TextField(default="This is home")
+    location = models.CharField(max_length=60, choices=loc)
     police = models.CharField(max_length=10, default='0700000000')
     health = models.CharField(max_length=10, default='0700000000')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
 
     def __str__(self):
         return self.name
@@ -40,17 +38,17 @@ class Neighbour(models.Model):
         return hood
 
     @classmethod
-    def search_by_title(cls,search_term):
+    def search_by_title(cls, search_term):
         hood = cls.objects.filter(name__icontains=search_term)
         return hood
 
 
-    
 class Profile(models.Model):
     """
     Class that contains Profile details
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
     hood = models.ForeignKey(Neighbour, null='True')
 
     @receiver(post_save, sender=User)
@@ -79,14 +77,13 @@ class Profile(models.Model):
         return profile
 
 
-   
 class Business(models.Model):
     """
-    Class that contains Business details
+    Contains Business details
     """
-    name = models.CharField(max_length = 30)
-    description = models.TextField(default="Check out my business")
-    email = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
+    description = models.TextField(default="business")
+    email = models.CharField(max_length=30)
     user = models.ForeignKey(User)
     hood = models.ForeignKey(Neighbour)
 
@@ -108,26 +105,23 @@ class Business(models.Model):
     def get_biz_by_hood(cls, id):
         biz = Business.objects.filter(hood_id=id).all()
         return biz
-            
+
     @classmethod
     def search_biz(cls, name):
         biz = cls.objects.filter(name__icontains=name)
         return biz
 
+
 class Join(models.Model):
-    """
-    Class that contains monitors which users have joined which neighbourhoods
-    """
-    user = models.OneToOneField(User)
+ models.OneToOneField(User)
     hood = models.ForeignKey(Neighbour)
 
     def __str__(self):
         return self.user
 
+
 class Posts(models.Model):
-    """
-    Class that defines Posts and their properties
-    """
+
     body = models.TextField()
     user = models.ForeignKey(User)
     hood = models.ForeignKey(Neighbour)
